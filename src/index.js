4c72db7296bad8b9a883d89e927780062cafd989
Adrian <blackrebel.068@gmail.com>
2022-12-28 13:54:31 -0600

    added file dev

--- src/index.js
+++ src/index.js
@@ -1,15 +1,4 @@
-import express from "express";
-import employeesRoutes from "./routes/employees.routes.js";
-import indexRoutes from "./routes/index.routes.js";
-
-const app = express();
-
-app.use(express.json());
-
-app.use('/api',employeesRoutes);
-app.use('/api',indexRoutes);
-
-app.use((req,res,next) => res.status(404).json({message:'endpoint not found'}))
-
-app.listen(3000);
-console.log("Listening on port");
+import app from"./app.js";
+import { PORT } from "./config.js";
+app.listen(PORT);
+console.log("Listening on port", PORT);
