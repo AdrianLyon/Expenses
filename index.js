e80f2497cb907b0be147da4d1911f416b96a3e63
Adrian <blackrebel.068@gmail.com>
2022-12-28 10:34:20 -0600

    I added the endpoint employees

--- index.js
+++ index.js
@@ -2,5 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get("/employees", (req, res) => res.send("get employeese"));
+app.post("/employees", (req, res) => res.send("create employeese"));
+app.put("/employees", (req, res) => res.send("update employeese"));
+app.delete("/employees", (req, res) => res.send("delete employeese"));
+
 app.listen(3000);
 console.log("Listening on port");
