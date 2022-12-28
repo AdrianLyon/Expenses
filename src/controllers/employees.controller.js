9d8fa50c6790c43c074e8b72086c89be74ac6cb9
Adrian <blackrebel.068@gmail.com>
2022-12-28 12:30:32 -0600

    I added get a employee by id

--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -18,5 +18,10 @@ export const createEmployee = async (req, res) => {
   });
 };
 
+export const getEmployee = async (req, res) => {
+  const [rows] = await pool.query("SELECT id, name,salary FROM employee WHERE id = ?", [req.params.id]);
+  if (rows.length <= 0) return res.status(404).json({ message:'Employee not found' });
+  res.json(rows[0]);
+};
 export const updateEmployee = (req, res) => res.send("update employeese");
 export const deleteEmployee = (req, res) => res.send("delete employeese");
